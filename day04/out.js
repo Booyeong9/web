1ccf3f88b642996daf03443540c2d18f47aa4e78
Booyeong9 <124110987+Booyeong9@users.noreply.github.com>
2023-02-02 17:41:22 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,21 @@
+// 함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의 
+function 빨래하다() {
+    console.log('1. 먼지를 털다')
+    console.log('2. 물에 넣는다')
+    console.log('3. 비누 바른다')
+    console.log('4. 빤다')
+    console.log('5. 행군다')
+    console.log('6. 짠다.')
+    console.log('7. 넌다.')
+  }
+  //  소괄호 () 함수 쓸 때 반드시 사용
+ // 함수 사용, 기능 사용
+
+  function 청소하다 () {
+
+    document.write ('1. 청소기 돌린다.')
+    document.write ('2. 돌돌이를 쓴다.')
+    document.write ('3. 물청소를 한다.')
+    document.write ('4. 창문을 연다.')
+    
+  }
---
+++ day04/out2.js
@@ -0,0 +1,13 @@
+function 여행하다() {
+    document.write ('1. 비행기를 타다' + '<br>')
+    document.write ('2. 맛집을 가다'+ '<br>')
+    document.write ('3. 사진을 찍다'+ '<br>')
+    document.write ('4. 숙소를 가다'+ '<br>')
+}
+
+function 쉬다() {
+    document.write ('1. 잠을 자다'+ '<br>')
+    document.write ('2. 휴대폰을 보다'+ '<br>')
+    document.write ('3. 유튜브를 보다'+ '<br>')
+    document.write ('4. 게임을 한다'+ '<br>')
+}
